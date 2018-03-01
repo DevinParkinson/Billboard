bc8c265c9e584f48ff81d87342318b8d0ebf2383
Ian Harwell <sirharwell@gmail.com>
2018-03-01 16:32:55 -0700

    index

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,13 +1,32 @@
 class ArtistsController < ApplicationController
-  def index
-  end
+  before_action :set_song
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
   def new
+    @artist = @song.artists.new
   end
 
-  def show
+  def create
+    @artist = @song.artists.new(artist_params)
+    if @artist.save
+      redirect_to board_song_path(@song)
+    else
+      render :new
+    end
   end
 
-  def edit
+  private
+
+  def set_song
+    @song = Song.find(params[:song_id])
+  end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+
+
+  def artist_params
+    params.require(:artist).permit(:name, :song_id)
   end
 end
