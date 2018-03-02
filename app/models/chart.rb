b92a2a8dc0eab6c70c97c5dd4b996402f74ac52c
Devin Parkinson <dparky13@icloud.com>
2018-03-02 15:47:26 -0700

    actually able to destroy EVERYTHING

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,4 +1,4 @@
 class Chart < ApplicationRecord
-  has_many :artists dependent: :destroy
-  has_many :songs dependent: :destroy
+  has_many :artists, dependent: :destroy
+  has_many :songs, dependent: :destroy
 end
