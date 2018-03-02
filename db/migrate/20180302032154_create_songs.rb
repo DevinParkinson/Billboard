3c0f0a3eabf55010c309c862640ef2053bf0f910
Devin Parkinson <dparky13@icloud.com>
2018-03-01 20:50:48 -0700

    s

--- db/migrate/20180302032154_create_songs.rb
+++ db/migrate/20180302032154_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration[5.1]
   def change
     create_table :songs do |t|
       t.string :name
-      t.belongs_to :artists, foreign_key: true
+      t.belongs_to :artist, foreign_key: true
       t.belongs_to :chart, foreign_key: true
 
       t.timestamps
